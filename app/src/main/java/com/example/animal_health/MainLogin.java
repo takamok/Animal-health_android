f60244afa1dddb4562c9ab6fad3bb481bdc1de60
ellaayu <ella.ayuningtyas.eru@gmail.com>
2019-11-06 15:48:02 +0900

    Logout, Google SignIn 키를 변경

--- app/src/main/java/com/example/animal_health/MainLogin.java
+++ app/src/main/java/com/example/animal_health/MainLogin.java
@@ -1,25 +1,39 @@
 package com.example.animal_health;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 //import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainLogin extends AppCompatActivity {
     private static final String TAG = "AndroidClarified";
+    public static final String GOOGLE_ACCOUNT = "google_account";
     private GoogleSignInClient googleSignInClient;
     private SignInButton googleSignInButton;
+    private TextView profileName, profileEmail;
+    private Button signOut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         googleSignInButton = (SignInButton) findViewById(R.id.sign_in_button);
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
@@ -32,7 +46,79 @@ public class MainLogin extends AppCompatActivity {
                 startActivityForResult(signInIntent, 101);
             }
         });
+//        signOut.setVisibility(View.VISIBLE);
+        profileName = findViewById(R.id.profile_text);
+        profileEmail = findViewById(R.id.profile_email);
+        signOut=findViewById(R.id.sign_out);
 
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+          /*
+          Sign-out is initiated by simply calling the googleSignInClient.signOut API. We add a
+          listener which will be invoked once the sign out is the successful
+           */
+                //        GoogleSignInClient googleSignInClient = null;
+                googleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        //On Succesfull signout we navigate the user back to LoginActivity
+                        Intent intent=new Intent(MainLogin.this,MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+
+                    }
+                });
+            }
+        });
+
+    }
+//    private void setDataOnView() {
+//        GoogleSignInAccount googleSignInAccount = getIntent().getParcelableExtra(GOOGLE_ACCOUNT);
+//        //Picasso.get().load(googleSignInAccount.getPhotoUrl()).centerInside().fit().into(profileImage);
+//        profileName.setText(googleSignInAccount.getDisplayName());
+//        profileEmail.setText(googleSignInAccount.getEmail());
+//    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK)
+            switch (requestCode) {
+                case 101:
+                    try {
+                        // The Task returned from this call is always completed, no need to attach
+                        // a listener.
+                        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                        GoogleSignInAccount account = task.getResult(ApiException.class);
+                        onLoggedIn(account);
+                    } catch (ApiException e) {
+                        // The ApiException status code indicates the detailed failure reason.
+                        Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+                    }
+                    break;
+            }
+    }
+
+    private void onLoggedIn(GoogleSignInAccount googleSignInAccount) {
+//        Intent intent = new Intent(this, ProfileActivity.class);
+       // intent.putExtra(MainLogin.GOOGLE_ACCOUNT, googleSignInAccount);
+        profileName.setText(googleSignInAccount.getDisplayName());
+        profileEmail.setText(googleSignInAccount.getEmail());
+        googleSignInButton.setVisibility(View.INVISIBLE);
+       signOut.setVisibility(View.VISIBLE);
+//        startActivity(intent);
+//        finish();
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        GoogleSignInAccount alreadyloggedAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if (alreadyloggedAccount != null) {
+            Toast.makeText(this, "Already Logged In", Toast.LENGTH_SHORT).show();
+            onLoggedIn(alreadyloggedAccount);
+        } else {
+            Log.d(TAG, "Not logged in");
+        }
     }
 }
 
