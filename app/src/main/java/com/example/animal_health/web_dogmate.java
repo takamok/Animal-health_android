ad975e8f4bf1ed2dd1e39067ecee5ffddf584661
ellaayu <ella.ayuningtyas.eru@gmail.com>
2019-11-11 10:08:44 +0900

    map

---
+++ app/src/main/java/com/example/animal_health/web_dogmate.java
@@ -0,0 +1,36 @@
+package com.example.animal_health;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class web_dogmate extends AppCompatActivity {
+    private WebView webView;
+    private WebSettings webSettings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_dogmate);
+
+        webView = (WebView)findViewById(R.id.dogmate);
+
+        webView.setWebViewClient(new WebViewClient());
+        webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setSupportMultipleWindows(false);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(false);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setSupportZoom(false);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        webSettings.setDomStorageEnabled(true);
+
+        webView.loadUrl("https://www.dogmate.co.kr/");
+    }
+}
